08340bbd9d0d89be741370040b3ced7d993daa30
Pål Rune Sørensen Tuv <me@paaltuv.no>
2021-09-20 16:08:01 -0500

    Update BaseHashIdConverter.cs

--- src/Snakk.API/Helpers/HashIdConverters/BaseHashIdConverter.cs
+++ src/Snakk.API/Helpers/HashIdConverters/BaseHashIdConverter.cs
@@ -15,30 +15,30 @@ namespace Snakk.API.Helpers.HashIdConverters
     {
         private readonly Hashids _hashIds;
 
-        public BaseHashIdConverter(string salt)
-        {
-            _hashIds = new Hashids(salt);
-        }
+        public BaseHashIdConverter(string salt) 
+            => _hashIds = new Hashids(salt);
 
         public long GetIdFromHash(string hash)
         {
+            if (string.IsNullOrEmpty(hash))
+                throw new IdToHashIdConvertionException();
+            
             var ids = _hashIds.DecodeLong(hash);
 
             if (ids == null || ids.Length != 1)
-            {
                 throw new HashIdToIdConvertionException();
-            }
 
             return ids[0];
         }
 
         public string GetHashFromId(long id) {
+            if (id == 0) 
+                throw new IdToHashIdConvertionException();
+            
             var hash = _hashIds.EncodeLong(id);
 
-            if (string.IsNullOrEmpty(hash))
-            {
+            if (string.IsNullOrEmpty(hash)) 
                 throw new IdToHashIdConvertionException();
-            }
 
             return hash;
         }
