6837aa298d1be6dcf604b15fbb6c02f9a140cf3b
Pål Rune S. Tuv <me@paaltuv.no>
2021-09-27 14:02:59 -0500

    Added missing property

--- src/Snakk.API.QueryResult.Dto/Routes/Comment/Services/Get/CommentDto.cs
+++ src/Snakk.API.QueryResult.Dto/Routes/Comment/Services/Get/CommentDto.cs
@@ -1,11 +1,16 @@
 ﻿//  SPDX-FileCopyrightText: 2021 Pål Rune Sørensen Tuv <me@paaltuv.no>
 //  SPDX-License-Identifier: MIT
 
+using System;
+
 namespace Snakk.API.QueryResult.Dto.Routes.Comment.Services.Get
 {
     public class CommentDto : BasePluginDataDto
     {
         public long Id { get; set; }
+
         public string Text { get; set; }
+
+        public DateTime CreatedUtc { get; set; }
     }
 }
