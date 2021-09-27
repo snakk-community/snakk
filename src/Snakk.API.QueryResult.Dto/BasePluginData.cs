c5d6e21eb3ea96eed1ffc84e4d09fd451ad35a2e
Pål Rune S. Tuv <me@paaltuv.no>
2021-09-27 13:55:56 -0500

    added header comments

--- src/Snakk.API.QueryResult.Dto/BasePluginData.cs
+++ src/Snakk.API.QueryResult.Dto/BasePluginData.cs
@@ -1,4 +1,7 @@
-﻿namespace Snakk.API.QueryResult.Dto
+﻿//  SPDX-FileCopyrightText: 2021 Pål Rune Sørensen Tuv <me@paaltuv.no>
+//  SPDX-License-Identifier: MIT
+
+namespace Snakk.API.QueryResult.Dto
 {
     public class BasePluginDataDto
     {
--- src/Snakk.API.QueryResult.Dto/Routes/Comment/Services/Get/CommentDto.cs
+++ src/Snakk.API.QueryResult.Dto/Routes/Comment/Services/Get/CommentDto.cs
@@ -1,4 +1,7 @@
-﻿namespace Snakk.API.QueryResult.Dto.Routes.Comment.Services.Get
+﻿//  SPDX-FileCopyrightText: 2021 Pål Rune Sørensen Tuv <me@paaltuv.no>
+//  SPDX-License-Identifier: MIT
+
+namespace Snakk.API.QueryResult.Dto.Routes.Comment.Services.Get
 {
     public class CommentDto : BasePluginDataDto
     {
