106691e583423e4f11882d11c995283f901f4b20
Pål Rune S. Tuv <me@paaltuv.no>
2021-09-27 13:54:54 -0500

    Removed test code

--- src/Snakk.API/Routes/Comment/Services/Get.cs
+++ src/Snakk.API/Routes/Comment/Services/Get.cs
@@ -3,12 +3,7 @@
 
 using Snakk.API.Helpers;
 using SqlKata.Execution;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Dynamic.Core;
-using System.Linq.Expressions;
-using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Snakk.API.Routes.Comment.Services
@@ -108,36 +103,5 @@ namespace Snakk.API.Routes.Comment.Services
                 pluginEnumerable,
                 i => i.CommentQueryBuilderAfter(commentId, commentQueryResultDto));
         #endregion
-    }
-
-    public class SelectBuilder<TSource>
-    {
-        private readonly List<MemberInfo> members = new List<MemberInfo>();
-
-        public SelectBuilder<TSource> Add<TValue>(Expression<Func<TSource, TValue>> selector)
-        {
-            var member = ((MemberExpression)selector.Body).Member;
-
-            members.Add(member);
-
-            return this;
-        }
-
-        public IQueryable<TResult> Select<TResult>(IQueryable<TSource> source)
-        {
-            var sourceType = typeof(TSource);
-            var resultType = typeof(TResult);
-
-            var parameter = Expression.Parameter(sourceType, "e");
-
-            var bindings = members.Select(member => Expression.Bind(
-                resultType.GetProperty(member.Name), Expression.MakeMemberAccess(parameter, member)));
-
-            var body = Expression.MemberInit(Expression.New(resultType), bindings);
-
-            var selector = Expression.Lambda<Func<TSource, TResult>>(body, parameter);
-
-            return source.Select(selector);
-        }
     }
 }
