673f6f054ff0165204c8a77f89ce735f0fc53125
Pål Rune S. Tuv <me@paaltuv.no>
2021-09-27 18:25:07 -0500

    Fixed path

--- src/Snakk.API/Routes/BaseService.cs
+++ src/Snakk.API/Routes/BaseService.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿//  SPDX-FileCopyrightText: 2021 Pål Rune Sørensen Tuv <me@paaltuv.no>
+//  SPDX-License-Identifier: MIT
+
+using System.Collections.Generic;
 
 namespace Snakk.API.Routes
 {
